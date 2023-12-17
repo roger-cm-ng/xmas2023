26b219f6749cd1525d7eeb656e3bf90399ecdf03
Roger Ng <roger.ng@3plearning.com>
2023-12-17 22:26:15 +1100

    host files

--- src/phaser/preload-scene.js
+++ src/phaser/preload-scene.js
@@ -6,11 +6,9 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload () {
-    this.load.image('prawn', './assets/prawn-small.png');
-    this.load.image('game-over', './assets/game-over.png');
-    this.load.image('rectangle', './assets/rectangle.png');
-    this.load.image('santa-big-mouth', './assets/santa.png');
-    this.load.audio('game-over', './assets/game-over.mp3');
+    this.load.image('prawn', 'https://cdn.jsdelivr.net/gh/roger-cm-ng/xmas2023/assets/prawn-small.png');
+    this.load.image('rectangle', 'https://cdn.jsdelivr.net/gh/roger-cm-ng/xmas2023/assets/rectangle.png');
+    this.load.image('santa-big-mouth', 'https://cdn.jsdelivr.net/gh/roger-cm-ng/xmas2023/assets/santa.png');
   }
 
   create () {
